542086afd588f8cb3c31c86bc65200c056c51930
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-03-26 20:34:34 +0200

    Uncomment mocking data

--- src/data/loadingZones.js
+++ src/data/loadingZones.js
@@ -1,64 +1,64 @@
-// const loadingZones = [
-//   {
-//     name: "Zone 1",
-//     id: 1,
-//   },
-//   {
-//     name: "Zone 2",
-//     id: 2,
-//   },
-//   {
-//     name: "Zone 3",
-//     id: 3,
-//   },
-//   {
-//     name: "Zone 4",
-//     id: 4,
-//   },
-//   {
-//     name: "Zone 5",
-//     id: 5,
-//   },
-//   {
-//     name: "Zone 6",
-//     id: 6,
-//   },
-//   {
-//     name: "Zone 7",
-//     id: 7,
-//   },
-//   {
-//     name: "Zone 8",
-//     id: 8,
-//   },
-//   {
-//     name: "Zone 9",
-//     id: 9,
-//   },
-//   {
-//     name: "Zone 10",
-//     id: 10,
-//   },
-//   {
-//     name: "Zone 11",
-//     id: 11,
-//   },
-//   {
-//     name: "Zone 12",
-//     id: 12,
-//   },
-//   {
-//     name: "Zone 13",
-//     id: 13,
-//   },
-//   {
-//     name: "Zone 14",
-//     id: 14,
-//   },
-//   {
-//     name: "Zone 15",
-//     id: 15,
-//   },
-// ];
+const loadingZones = [
+  {
+    name: "Zone 1",
+    id: 1,
+  },
+  {
+    name: "Zone 2",
+    id: 2,
+  },
+  {
+    name: "Zone 3",
+    id: 3,
+  },
+  {
+    name: "Zone 4",
+    id: 4,
+  },
+  {
+    name: "Zone 5",
+    id: 5,
+  },
+  {
+    name: "Zone 6",
+    id: 6,
+  },
+  {
+    name: "Zone 7",
+    id: 7,
+  },
+  {
+    name: "Zone 8",
+    id: 8,
+  },
+  {
+    name: "Zone 9",
+    id: 9,
+  },
+  {
+    name: "Zone 10",
+    id: 10,
+  },
+  {
+    name: "Zone 11",
+    id: 11,
+  },
+  {
+    name: "Zone 12",
+    id: 12,
+  },
+  {
+    name: "Zone 13",
+    id: 13,
+  },
+  {
+    name: "Zone 14",
+    id: 14,
+  },
+  {
+    name: "Zone 15",
+    id: 15,
+  },
+];
 
-// export default loadingZones;
+export default loadingZones;
